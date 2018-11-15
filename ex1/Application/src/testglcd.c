1972b6f6c28794748fb6d9fa9455d2ff0e16dd8f
Markus Krainz (uC) <ldm@gmx.at>
2018-11-15 22:08:03 +0100

    Can compile with wiiUserInit stubs

--- ex1/Application/src/testglcd.c
+++ ex1/Application/src/testglcd.c
@@ -3,7 +3,20 @@
 #include "wii_user.h"
 #include <stdbool.h>
 
-void setup() {}
+void rcvButton(uint8_t wii, uint16_t buttonStates) {}
+void rcvAccel(uint8_t wii, uint16_t x, uint16_t y, uint16_t z) {}
+void conCallback(uint8_t wii, connection_status_t status) {}
+
+void setup() {
+  DDRA = 0xFF;
+  PORTA = 0xFF;
+
+  uint8_t wii = 1;
+  const uint8_t mac = 1;
+
+  error_t ret = wiiUserInit(&rcvButton, &rcvAccel);
+  ret = wiiUserConnect(wii, &mac, &conCallback);
+}
 
 void background() {}
 
---
+++ ex1/Application/src/uart.c
@@ -0,0 +1,4 @@
+#include "uart.h"
+
+error_t halWT41FcUartInit(void (*sndCallback)(), void (*rcvCallback)(uint8_t)) { return SUCCESS; }
+error_t halWT41FcUartSend(uint8_t byte) { return SUCCESS; }
