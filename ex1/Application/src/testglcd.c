787781475414526b88e5fa434cdc91e1ca867d7c
Markus Krainz (uC) <ldm@gmx.at>
2018-11-15 03:52:31 +0100

    Further improve Makefile

--- ex1/Application/src/testglcd.c
+++ ex1/Application/src/testglcd.c
@@ -1,6 +1,20 @@
 #include "glcd.h"
+#include <stdbool.h>
+#include "rand.h"
+
+void background(){
+    
+}
 
 int main (void){
+    glcdInit();
+    xy_point p1 = {5,20}, p2 = {80,80};
+
+    glcdDrawLine(p1 , p2, &glcdInvertPixel);
+
+while (true){
+    background();
+}
 
     return 0;
 }
