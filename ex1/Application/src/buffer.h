37b08d38b4f8868d7cfbcc0cf57d08bb8e4d665a
Markus Krainz (uC) <ldm@gmx.at>
2018-11-17 18:00:52 +0100

    Make buffer always inline

--- ex1/Application/src/buffer.h
+++ ex1/Application/src/buffer.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "buffer.h"
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h> /* memset */
 
 // needs to be <=255
-#define BUF_SIZE 32
+#define BUF_SIZE 128
 
 typedef struct {
   uint8_t data[BUF_SIZE];
@@ -13,7 +15,84 @@ typedef struct {
   uint8_t writer;
 } ringbuffer;
 
-void buffer_init(ringbuffer *const buffer);
-uint8_t buffer_count(ringbuffer *const buffer);
-bool buffer_put(ringbuffer *const buffer, const uint8_t in);
-bool buffer_take(ringbuffer *const buffer, uint8_t *const out);
+void inline __attribute__((always_inline)) buffer_init(volatile ringbuffer *buffer);
+uint8_t inline __attribute__((always_inline)) buffer_count(volatile ringbuffer *buffer);
+bool inline __attribute__((always_inline))
+buffer_put(volatile ringbuffer *buffer, const uint8_t in);
+bool inline __attribute__((always_inline))
+buffer_take(volatile ringbuffer *buffer, uint8_t *const out);
+static inline __attribute__((always_inline)) uint8_t next(uint8_t current);
+static inline __attribute__((always_inline)) bool isFull(volatile ringbuffer *buffer);
+static inline __attribute__((always_inline)) bool isEmpty(volatile ringbuffer *buffer);
+
+// public methods
+
+uint8_t buffer_count(volatile ringbuffer *buffer) {
+  if (isFull(buffer)) {
+    return BUF_SIZE;
+  }
+
+  if (isEmpty(buffer)) {
+    return 0;
+  }
+
+  return (buffer->reader > buffer->writer) ? (buffer->reader - buffer->writer)
+                                           : (buffer->writer - buffer->reader);
+}
+
+void buffer_init(volatile ringbuffer *buffer) { memset((void *)buffer, 0, sizeof(ringbuffer)); }
+
+/**
+ *
+ * @return true if successful, retry on on false
+ */
+bool buffer_put(volatile ringbuffer *buffer, const uint8_t in) {
+  const bool full = isFull(buffer);
+  if (full) {
+    return false;
+  }
+
+  buffer->data[buffer->writer] = in;
+
+  buffer->writer = next(buffer->writer);
+  if (buffer->writer == buffer->reader) {
+    buffer->full = true;
+  }
+
+  return true;
+}
+
+bool buffer_take(volatile ringbuffer *buffer, uint8_t *const out) {
+  const bool empty = isEmpty(buffer);
+
+  if (empty) {
+    return false;
+  }
+
+  *out = buffer->data[buffer->reader];
+
+  buffer->reader = next(buffer->reader);
+  if (buffer->writer == buffer->reader) {
+    buffer->full = false;
+  }
+
+  return true;
+}
+
+// (private) static methods
+
+uint8_t next(uint8_t current) {
+  if (current == BUF_SIZE - 1) {
+    return 0;
+  } else {
+    return ++current;
+  }
+}
+
+bool isFull(volatile ringbuffer *buffer) {
+  return (buffer->reader == buffer->writer) && buffer->full;
+}
+
+bool isEmpty(volatile ringbuffer *buffer) {
+  return (buffer->reader == buffer->writer) && !(buffer->full);
+}
